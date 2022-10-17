cb780c86d8f14ed50fc002376b6629b1e4481283
Ahmed-ALU <a.mohamed@alustudent.com>
2022-10-17 09:11:40 +0200

    New Video

---
+++ Arduino/Lesson01_Tutorial/Lesson01_Tutorial.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(800);
+  digitalWrite(8, HIGH);
+  delay(700);
+  digitalWrite(8, LOW);
+  delay(600);
+  digitalWrite(8, HIGH);
+  delay(500);
+  digitalWrite(8, LOW);
+  delay(400);
+  digitalWrite(8, HIGH);
+  delay(300);
+  digitalWrite(8, LOW);
+  delay(200);
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(50);
+  digitalWrite(8, HIGH);
+  delay(25);
+  digitalWrite(8, LOW);
+}
